6439bd1cb71c40c88fadd49213f905272882c450
lgapinski <lukasz.gapinski18@gmail.com>
2025-04-06 18:39:22 +0200

    Utworzenie projektu serwera

---
+++ ProjectLayerClassServerLibraryTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ProjectLayerClassServerLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
